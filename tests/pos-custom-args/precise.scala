0212edcfe0129773db0f0090f54761101d3f3afc
Matt Bovel <matthieu@bovel.net>
2022-10-10 20:16:05 +0200

    Add examples

--- tests/pos-custom-args/precise.scala
+++ tests/pos-custom-args/precise.scala
@@ -1,11 +1,35 @@
+import scala.compiletime.ops.int.*
 
-dependent case class Foo(v: Int)
+dependent class Vec(val size: Int):
+  dependent def ++(v2: Vec) = Vec(size + v2.size)
+  def zip(v2: Vec(size.type)) = Vec(size)
 
-dependent def precise() =
-  val v1 = 1
-  val v2 = 2 + v1
-  dependent def isString(x: Any) = x match
-    case _: String => true
-    case _ => false
-  val v3 = isString(42)
-  val v4 = Foo(42)
+def vecSipExample =
+  val v1 = Vec(1)
+  val v1T: Vec(1) = v1
+  val v2 = Vec(2)
+  val v2T: Vec(2) = v2
+  val v3 = v1T ++ v2T
+  val v3T: Vec(3) = v3
+  val one = 1
+  val vOne = Vec(one)
+  val vOneT: Vec(one.type) = vOne
+  val vTwo = Vec(one + 1)
+  val vTwoT: Vec = vTwo
+  val vThree = vOneT ++ vTwoT
+  val vThreeT: Vec = vThree
+
+
+def letAbstractionNormal =
+  val n: Int = ???
+  val nPlus1 /*: Int*/ = n + 1
+  Vec(n + 1).zip(Vec(nPlus1))
+  //             ^^^^^^^^^^^
+  //             Found:    Vec((nPlus1 : Int))
+  //             Required: Vec((?1 : Int))
+  //             where:    ?1 is an unknown value of type Int
+
+dependent def letAbstractionDependent =
+  val n: Int = ???
+  val nPlus1 /*: n + 1*/ = n + 1
+  Vec(n + 1).zip(Vec(nPlus1))
