c8898fda8f3fd7fa946ad05438808251ae965c03
Quentin Bernet <quentin.bernet@epfl.ch>
2023-06-20 13:14:06 +0200

    Revert "Add potentially superfluous parentheseses in postFixLambda example"
    This reverts commit f3b745feded82f2927a385b3c185cf1dd56c410b.

--- tests/pos-custom-args/refinements/jsonSchemasPostfixLambda.scala
+++ tests/pos-custom-args/refinements/jsonSchemasPostfixLambda.scala
@@ -60,7 +60,7 @@ type Product = Dict with d =>
   required("productId")[Int] &&
   required("productName")[String] &&
   required("price")[Double with _ > 0] &&
-  optional("tags")[List[String] with (a => a.size >= 1 && unique(a))] &&
+  optional("tags")[List[String] with a => a.size >= 1 && unique(a)] &&
   optional("dimensions")[Dimensions]
 
 type Dimensions = Dict with d =>
@@ -95,9 +95,9 @@ object usingCaseClass:
 object usingImplicits:
   type LongitudeAndLatitudeValues = Dict with d =>
     given Dict = d
-    required("latitude")[Double with (l => -90 <= l && l <= 90)] &&
-    required("longitude")[Double with (l => -180 <= l && l <= 180)] &&
-    required("city")[String with (city => raw"^[A-Za-z . ,'-]+$$".r.matches(city))]
+    required("latitude")[Double with l => -90 <= l && l <= 90] &&
+    required("longitude")[Double with l => -180 <= l && l <= 180] &&
+    required("city")[String with city => raw"^[A-Za-z . ,'-]+$$".r.matches(city)]
 
 // Enumerations - String
 
