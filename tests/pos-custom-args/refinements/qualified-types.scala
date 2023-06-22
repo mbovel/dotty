80d821eb81a234d718f786a37174f38af63b6ec0
Quentin Bernet <quentin.bernet@epfl.ch>
2023-06-22 14:38:18 +0200

    Add and update tests

---
+++ tests/pos-custom-args/refinements/qualified-types.scala
@@ -0,0 +1,18 @@
+
+object setNotation:
+  import language.experimental.setNotation
+
+object postfixLambda:
+  import language.experimental.postfixLambda
+  val b1 = true
+  val b2 = true
+
+  type T1 = Int with b1 && b2
+  type T2 = Int with x => b1 && b2
+  type T3 = Int with x =>
+      b1 &&
+      b2
+  type T4 = Int with
+    x =>
+      b1 &&
+      b2
