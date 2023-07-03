e83979ae1e4f2fb89db196ffaba915936b402475
Quentin Bernet <quentin.bernet@epfl.ch>
2023-07-03 13:33:57 +0200

    Refactor patmat test

--- tests/run-custom-args/qualified-types/patmat.scala
+++ tests/run-custom-args/qualified-types/patmat.scala
@@ -10,32 +10,51 @@ type PoliteString = {s: NonEmptyString with s.head.isUpper && s.takeRight(6) == 
 type Pos = {x: Int with x > 0}
 val scrut: Any = 4
 
+inline val isPosTrue = "It Pos"
+inline val isPosFalse = "It not Pos"
+
+inline val isPoliteTrue = "It Polite"
+inline val isPoliteFalse = "It Impolite"
+
+
+
+
 @main
 def Test =
-  println(isPos(4))
-  println(isPos(0))
-  println(isPos(-1))
 
-  println(isPos("hello"))
-  println(isPos('c'))
-  println(isPos(List()))
+  val examples = List( // val, isPosBool, isPoliteBool
+    ("hello",                       false, false),
+    ('c',                           false, false),
+    (List(),                        false, false),
 
-  println(isPolite("Give me the butter, please"))
-  println(isPolite("give me the butter, please"))
-  println(isPolite("Give me the butter"))
-  println(isPolite("Give me the butter"))
-  println(isPolite("")) // if checks were not done in correct order, it would result in "".head which would fail at runtime
-  println(isPolite(72))
+    (72,                            true,  false),
+    (4,                             true,  false),
+    (0,                             false, false),
+    (-1,                            false, false),
+
+    ("Give me the butter, please",  false, true ),
+    ("give me the butter, please",  false, false),
+    ("Give me the butter",          false, false),
+    ("give me the butter",          false, false),
+    ("",                            false, false), // if checks were not done in correct order, it would result in "".head which would fail at runtime
+  )
+
+  examples.foreach: (v, expectedPosBool, expectedPoliteBool) =>
+    val expectedPosStr = if expectedPosBool then isPosTrue else isPosFalse
+    assert( v.isInstanceOf[Pos] == expectedPosBool )
+    assert( isPos(v) == expectedPosStr )
+
+    val expectedPoliteStr = if expectedPoliteBool then isPoliteTrue else isPoliteFalse
+    assert( v.isInstanceOf[PoliteString] == expectedPoliteBool )
+    assert( isPolite(v) == expectedPoliteStr )
+
+    println(s"$v: ${isPos(v)}, and ${isPolite(v)}")
+
 
 def isPos(x: Any) = x match
-  case x: Pos => "It Pos"
-  case _ => "It not Pos"
-
-def isPos2(x: Int) = x match
-  case x: Pos => "It Pos"
-  case _ => "It not Pos"
-
+  case x: Pos => isPosTrue
+  case _ => isPosFalse
 
 def isPolite(x: Any) = x match
-  case x: PoliteString => "It Polite"
-  case _ => "It Impolite"
+  case x: PoliteString => isPoliteTrue
+  case _ => isPoliteFalse
