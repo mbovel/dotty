09948b7d5d5b8c0938811a51918001c8c6a79ba2
Matt Bovel <matthieu@bovel.net>
2024-03-06 11:56:45 +0100

    Add TODO

--- tests/pos-custom-args/qualified-types/runtime-check-1.scala
+++ tests/pos-custom-args/qualified-types/runtime-check-1.scala
@@ -1,5 +1,11 @@
-extension (x: Any) def runtimeChecked: x.type = x
+// TODO(Valentin889):
+// 1. move this extension method to the Scala 3 lib.
+//   - add it somewhere in /library/src
+//   - reference it from Definitions.scala (see TailrecAnnot as an example)
+// 2. special-case CheckQualifiedTypes to not throw a type error when the actual
+//    type of `x.runtimeChecked` does not conform to the expected type.
 
+extension (x: Any) def runtimeChecked: x.type = x
 
 def main =
   val x: Int = ???
