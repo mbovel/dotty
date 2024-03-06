3d5436cbaff05b2c92df29d41bc5fc13af477cc9
Matt Bovel <matthieu@bovel.net>
2024-03-06 11:56:45 +0100

    Add TODO

--- tests/pos-custom-args/qualified-types/runtime-check-1.scala
+++ tests/pos-custom-args/qualified-types/runtime-check-1.scala
@@ -1,5 +1,17 @@
-extension (x: Any) def runtimeChecked: x.type = x
+// TODO(Valentin889):
+// 1. move this extension method to the Scala 3 lib.
+//   - add it somewhere in /library/src
+//   - reference it from Definitions.scala (see TailrecAnnot as an example)
+// 2. special-case CheckQualifiedTypes to not throw a type error when the actual
+//    type of `x.runtimeChecked` does not conform to the expected type. This
+//    should probably be done in the method checkConformsExpr that should be
+//    overridden in CheckQualifiedTypes. There, you should match `tree` to check
+//    if it is an application of `runtimeChecked`. Something like:
+//
+//     tree match case Apply(fun, ...) if fun.symbol == defn.runtimeChecked =>
+//          ...
 
+extension (x: Any) def runtimeChecked: x.type = x
 
 def main =
   val x: Int = ???
