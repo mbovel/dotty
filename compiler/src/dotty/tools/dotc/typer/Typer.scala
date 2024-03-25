41fb24d9f4b37b99417cef6cf98903d0854a3664
Matt Bovel <matthieu@bovel.net>
2024-03-25 19:13:01 +0100

    Simplify propagatedFailure

--- compiler/src/dotty/tools/dotc/typer/Typer.scala
+++ compiler/src/dotty/tools/dotc/typer/Typer.scala
@@ -3849,22 +3849,9 @@ class Typer(@constructorOnly nestingLevel: Int = 0) extends Namer
         end implicitArgs
 
         val args = implicitArgs(wtp.paramInfos, 0, pt)
-
-        def propagatedFailure(args: List[Tree]): Type = args match {
-          case arg :: args1 =>
-            arg.tpe match {
-              case ambi: AmbiguousImplicits =>
-                propagatedFailure(args1) match {
-                  case NoType | (_: AmbiguousImplicits) => ambi
-                  case failed => failed
-                }
-              case failed: SearchFailureType => failed
-              case _ => propagatedFailure(args1)
-            }
-          case Nil => NoType
-        }
-
-        val propFail = propagatedFailure(args)
+        val firstNonAmbiguous = args.tpes.find(tp => tp.isError && !tp.isInstanceOf[AmbiguousImplicits])
+        def firstError = args.tpes.find(_.isError)
+        val propFail = firstNonAmbiguous.orElse(firstError).getOrElse(NoType)
 
         def issueErrors(): Tree = {
           def paramSymWithMethodTree(paramName: TermName) =
@@ -3897,8 +3884,9 @@ class Typer(@constructorOnly nestingLevel: Int = 0) extends Namer
           // need to be reset.
           ctx.typerState.resetTo(saved)
 
-          // If method has default params, fall back to regular application
-          // where all inferred implicits are passed as named args.
+          // If method has default params and there are no "Ambiguous implicits"
+          // error, fall back to regular application where all inferred
+          // implicits are passed as named args.
           if hasDefaultParams && !propFail.isInstanceOf[AmbiguousImplicits] then
             val namedArgs = wtp.paramNames.lazyZip(args).flatMap { (pname, arg) =>
               if (arg.tpe.isError) Nil else untpd.NamedArg(pname, untpd.TypedSplice(arg)) :: Nil
