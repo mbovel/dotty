24de9dcc497937b85b956b68a06e439197c6f446
Valentin Schneeberger <valentin2.schneeberger@gmail.com>
2024-05-17 16:24:34 +0200

    Add test for isInstanceOF

--- tests/run-custom-args/qualified-types/isInstanceOf-1.scala
+++ tests/run-custom-args/qualified-types/isInstanceOf-1.scala
@@ -1,16 +1,39 @@
 def f[T] = ()
 
 def Test =
-  val n1: Any = ???
+  val n: Any = ???
 
-  f[Int]
+  //this fail, the errors trigger radomly os I don't know where there is a problem
+  if n.isInstanceOf[{v: Int with v > 0}] then println("n is an Int with value > 0")
+  else println("n is not an Int with > 0")
 
-  if n1.isInstanceOf[Int] then println("n is an Int")
-  else println("n is not an Int")
+  /*
 
-  if n1.isInstanceOf[{v: Int with v == 10}] then println("n is an Int with value 10")
+  if n.isInstanceOf[{v: Int with v == 10}] then println("n is an Int with value 10")
   else println("n is not an Int with value 10")
 
-class C1:
-  def isInt =
-    if isInstanceOf[Int] then println("I am an Int")
+  if n.isInstanceOf[Int] then println("n is an Int")
+  else println("n is not an Int")
+
+
+  if n.isInstanceOf[{v: Int with v < 0}] then println("n is an Int with value < 0")
+  else println("n is not an Int with < 0")
+
+
+  if n.isInstanceOf[{v: String with v.isEmpty()}] then println("n is a empty String")
+  else println("n is a not an empty String")
+
+
+  if n.isInstanceOf[{v: String with v != ""}] then println("n is a not an empty String")
+  else println("n is an empty String")
+
+  */
+
+
+
+  /*
+    Fix to tree
+    Fix with string
+    Rename phases (runtimeChecked, eventuallyQualifiedType) put inside qualifier
+    Do pr on refinments not on dotty and don't deal with conflicts
+  */
